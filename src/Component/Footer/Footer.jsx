092aeeebefd6bce6529ccc4381be74a704e52950
ahmedosama24 <mrahmed2466@gmail.com>
2023-07-02 18:54:12 +0300

    footer

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='border-top container p-4 text-center fs-5 fw-light' style={{color:'var(--main-color)'}}>2023 RealAoe , Inc</div>
   )
 }
 
